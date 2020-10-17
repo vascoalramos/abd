4505de90a06b4c98d9f8996a482aeb0a39e31519
Vasco Ramos <vascoalramos@outlook.pt>
2020-10-17 15:36:40 +0100

    Add create table operation

--- benchmark/src/main/java/abd/Workload.java
+++ benchmark/src/main/java/abd/Workload.java
@@ -9,7 +9,10 @@ public class Workload {
     public static void populate(Random rand, Connection c) throws Exception {
         Statement s = c.createStatement();
 
-        // Criar tabelas e inserir valores
+        // create tables and insert values
+        s.executeUpdate("create table client (int id, name varchar, address varchar, data varchar)");
+        s.executeUpdate("create table product (int id, description varchar, data varchar)");
+        s.executeUpdate("create table invoice (int id, productId varchar, productId varchar, data varchar)");
 
         s.close();
     }
