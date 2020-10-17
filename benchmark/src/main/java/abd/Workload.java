d96663b243a1ee6a04e0b98a43a3f27a20d8e739
Vasco Ramos <vascoalramos@outlook.pt>
2020-10-17 17:02:05 +0100

    Add simple client insertion

--- benchmark/src/main/java/abd/Workload.java
+++ benchmark/src/main/java/abd/Workload.java
@@ -1,18 +1,22 @@
 package abd;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Random;
 
 public class Workload {
+    private static final int MAX = 1024; // n = 10 => MAX = 2^10 = 1024
+
     public static void populate(Random rand, Connection c) throws Exception {
         Statement s = c.createStatement();
 
         // create tables and insert values
-        s.executeUpdate("create table client (id int, name varchar, address varchar, data varchar)");
-        s.executeUpdate("create table product (id int, description varchar, data varchar)");
-        s.executeUpdate("create table invoice (id int, productId varchar, clientId varchar, data varchar)");
+        s.executeUpdate("create table client (id int, name varchar, address varchar, data varchar, primary key (id))");
+        s.executeUpdate("create table product (id int, description varchar, data varchar, primary key (id))");
+        s.executeUpdate("create table invoice (id int, productId int references product, clientId int references client, data varchar, primary key (id))");
+
+        // insert clients
+        s.executeUpdate("insert into client values ("+ generateId(rand) + ", 'vasco', 'ola', 'adeus')");
 
         s.close();
     }
@@ -23,5 +27,9 @@ public class Workload {
         // Executar operação
 
         s.close();
+    }
+
+    private static int generateId(Random rand) {
+        return rand.nextInt(MAX)|rand.nextInt(MAX);
     }
 }
