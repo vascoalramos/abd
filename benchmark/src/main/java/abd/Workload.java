2a349187e49a56f1890eed14605b59c9a49167a3
Vasco Ramos <vascoalramos@outlook.pt>
2020-10-19 15:51:51 +0100

    Update formatting

--- benchmark/src/main/java/abd/Workload.java
+++ benchmark/src/main/java/abd/Workload.java
@@ -11,14 +11,13 @@ import java.util.Random;
 public class Workload {
 	private static final int N = 10;
 	private static final int MAX = (int) Math.pow(2, N);
-
+	
 	// fields
 	private static final String NAME = RandomStringUtils.randomAlphabetic(10, 15);
 	private static final String ADDRESS = RandomStringUtils.randomAlphabetic(15, 25);
 	private static final String DATA = RandomStringUtils.randomAlphanumeric(1000, 1500);
 	private static final String DESCRIPTION = RandomStringUtils.randomAlphabetic(15, 100);
-
-
+	
 	public static void populate(Random rand, Connection c) throws SQLException {
 		Statement s = c.createStatement();
 		
@@ -26,17 +25,17 @@ public class Workload {
 		s.executeUpdate("create table client (id serial, name varchar, address varchar, data varchar, primary key (id))");
 		s.executeUpdate("create table product (id serial, description varchar, data varchar, primary key (id))");
 		s.executeUpdate("create table invoice (id serial, productId int references product, clientId int references client, data varchar, primary key (id))");
-
+		
 		// insert clients
 		for (int i = 0; i < MAX; i++) {
 			s.executeUpdate("insert into client (name, address, data) values ('" + NAME + "', '" + ADDRESS + "', '" + DATA + "')");
 		}
-
+		
 		// insert products
 		for (int i = 0; i < MAX; i++) {
 			s.executeUpdate("insert into product (description, data) values ('" + DESCRIPTION + "', '" + DATA + "')");
 		}
-
+		
 		// insert invoices
 		int clientId, productId;
 		for (int i = 0; i < Math.pow(2, N + 5); i++) {
@@ -44,13 +43,13 @@ public class Workload {
 			productId = generateId(rand);
 			s.executeUpdate("insert into invoice (productId, clientId, data) values ('" + clientId + "', '" + productId + "', '" + DATA + "')");
 		}
-
+		
 		s.close();
 	}
-
+	
 	public static void transaction(Random rand, Connection c) throws SQLException {
 		Statement s = c.createStatement();
-
+		
 		// execute random operation
 		int opt = rand.nextInt(3);
 		
@@ -70,7 +69,7 @@ public class Workload {
 		
 		s.close();
 	}
-
+	
 	private static void sell(Random rand, Statement s) throws SQLException {
 		int clientId = generateId(rand);
 		int productId = generateId(rand);
@@ -79,7 +78,7 @@ public class Workload {
 		
 		System.out.println("Product [" + productId + "] sold to client [" + clientId + "].");
 	}
-
+	
 	private static void account(Random rand, Statement s) throws SQLException {
 		int clientId = generateId(rand);
 		
@@ -93,8 +92,8 @@ public class Workload {
 			System.out.println("  -> Name: " + rs.getString("description"));
 		}
 		*/
-
-		System.out.println("");
+		
+		System.out.println();
 	}
 	
 	private static void top10(Statement s) throws SQLException {
@@ -109,8 +108,9 @@ public class Workload {
 		}
 		*/
 		
-		System.out.println("");
+		System.out.println();
 	}
+	
 	private static int generateId(Random rand) {
 		return rand.nextInt(MAX) | rand.nextInt(MAX);
 	}
