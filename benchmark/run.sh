5e291d97422145300f5cbbde46f221c63b4a9ad6
Vasco Ramos <vascoalramos@outlook.pt>
2020-10-20 19:46:15 +0100

    Fix project bench

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,8 +1,13 @@
+DB_URL='jdbc:postgresql://localhost/abd'
+DB_USER='vasco'
+DB_PWD='segredo'
+
 # generate jar executable
 mvn clean package jar:jar
 
 # run benchmark
-DB_URL='jdbc:postgresql://localhost/abd'
-DB_USER='vasco'
-DB_PWD='segredo'
-java -jar target/benchmark-1.0-SNAPSHOT.jar -d ${DB_URL} -U ${DB_USER} -P ${DB_PWD} -c 10 -x
+echo "\n\nPopulate\n"
+java -jar target/benchmark-1.0-SNAPSHOT.jar -d ${DB_URL} -U ${DB_USER} -P ${DB_PWD} -p
+
+echo "\n\nRun Benchmark\n"
+java -jar target/benchmark-1.0-SNAPSHOT.jar -d ${DB_URL} -U ${DB_USER} -P ${DB_PWD} -c 1 -x
--- benchmark/src/main/java/abd/Workload.java
+++ benchmark/src/main/java/abd/Workload.java
@@ -21,33 +21,34 @@ public class Workload {
 	public static void populate(Random rand, Connection c) throws SQLException {
 		Statement s = c.createStatement();
 		
+		// drop tables, if they exist
+		s.executeUpdate("drop table if exists client cascade;");
+		
+		s.executeUpdate("drop table if exists product cascade");
+		
+		s.executeUpdate("drop table if exists Invoice cascade");
+		
 		// create tables and insert values
-		s.executeUpdate("create table client (id serial, name varchar, address varchar, data varchar, primary key (id))");
-		s.executeUpdate("create table product (id serial, description varchar, data varchar, primary key (id))");
-		s.executeUpdate("create table invoice (id serial, productId int references product, clientId int references client, data varchar, primary key (id))");
+		s.executeUpdate("create table client (id int, name varchar, address varchar, data varchar)");
+		
+		s.executeUpdate("create table product (id int, description varchar, data varchar)");
+		
+		s.executeUpdate("create table invoice (id int, productId int, clientId int, data varchar)");
 		
 		// insert clients
 		for (int i = 0; i < MAX; i++) {
-			s.executeUpdate("insert into client (name, address, data) values ('" + NAME + "', '" + ADDRESS + "', '" + DATA + "')");
+			s.executeUpdate("insert into client (id, name, address, data) values ('" + i + "', '" + NAME + "', '" + ADDRESS + "', '" + DATA + "')");
 		}
 		
 		// insert products
 		for (int i = 0; i < MAX; i++) {
-			s.executeUpdate("insert into product (description, data) values ('" + DESCRIPTION + "', '" + DATA + "')");
-		}
-		
-		// insert invoices
-		int clientId, productId;
-		for (int i = 0; i < Math.pow(2, N + 5); i++) {
-			clientId = generateId(rand);
-			productId = generateId(rand);
-			s.executeUpdate("insert into invoice (productId, clientId, data) values ('" + clientId + "', '" + productId + "', '" + DATA + "')");
+			s.executeUpdate("insert into product (id, description, data) values ('" + i + "', '" + DESCRIPTION + "', '" + DATA + "')");
 		}
 		
 		s.close();
 	}
 	
-	public static void transaction(Random rand, Connection c) throws SQLException {
+	public static void transaction(Random rand, Connection c) throws Exception {
 		Statement s = c.createStatement();
 		
 		// execute random operation
@@ -68,45 +69,28 @@ public class Workload {
 		}
 		
 		s.close();
-		
-		System.out.println();
 	}
 	
-	private static void sell(Random rand, Statement s) throws SQLException {
+	private static void sell(Random rand, Statement s) throws Exception {
 		int clientId = generateId(rand);
 		int productId = generateId(rand);
 		
-		s.executeUpdate("insert into invoice (productId, clientId, data) values ('" + clientId + "', '" + productId + "', '" + DATA + "')");
+		ResultSet rs = s.executeQuery("select count(id) as total from invoice;");
+		rs.next();
 		
-		System.out.println("Product [" + productId + "] sold to client [" + clientId + "].");
+		int id = rs.getInt("total") + 1;
+		
+		s.executeUpdate("insert into invoice (id, productId, clientId, data) values ('" + id + "', '" + clientId + "', '" + productId + "', '" + DATA + "')");
 	}
 	
-	private static void account(Random rand, Statement s) throws SQLException {
+	private static void account(Random rand, Statement s) throws Exception {
 		int clientId = generateId(rand);
 		
-		ResultSet rs = s.executeQuery("select id, description from product inner join (select  distinct productId from invoice where clientId=" + clientId + ") as invoice on productId=product.id");
-		
-		System.out.println("List of products sold to client [" + clientId + "]:");
-		
-		// TODO: check this
-		/*
-		while (rs.next()) {
-			System.out.println("  -> Name: " + rs.getString("description"));
-		}
-		*/
+		s.executeQuery("select description from product inner join (select distinct productId from invoice where clientId=" + clientId + ") as invoice on productId=product.id");
 	}
 	
-	private static void top10(Statement s) throws SQLException {
-		ResultSet rs = s.executeQuery("select productId from invoice group by productId order by count(productId) desc limit 10;");
-		
-		System.out.println("Top 10 products:");
-		
-		// TODO: check this
-		/*
-		while (rs.next()) {
-			System.out.println("  -> ID: " + rs.getString("productid"));
-		}
-		*/
+	private static void top10(Statement s) throws Exception {
+		s.executeQuery("select productId from invoice group by productId order by count(productId) desc limit 10;");
 	}
 	
 	private static int generateId(Random rand) {
