b5425c7c371076ecfc608f82a8dc8ee70262c431
Vasco Ramos <vascoalramos@outlook.pt>
2020-11-05 18:03:23 +0000

    Update repo

--- benchmark/src/main/java/abd/Benchmark.java
+++ benchmark/src/main/java/abd/Benchmark.java
@@ -8,110 +8,108 @@ import java.sql.SQLException;
 import java.util.Random;
 
 public class Benchmark extends Thread {
-
-    private static final Options options = new Options();
-
-    private static boolean started, stopped;
-    private static int n, aborted;
-    private static long total;
-
-    private static final Random rand = new Random();
-
-    public synchronized static void startBench() {
-        started = true;
-
-        System.out.println("Started!");
-    }
-
-    public synchronized static void logTransaction(long tr, boolean success) {
-        if (started && !stopped) {
-            n++;
-            total += tr;
-            if (!success)
-                aborted += 1;
-        }
-    }
-
-    public synchronized static boolean isStopped() {
-        return stopped;
-    }
-
-    public synchronized static void stopBench() {
-        stopped = true;
-
-        System.out.println("throughput (txn/s) = " + (n / ((double) options.runtime)));
-        System.out.println("response time (s) = " + (total / (n * 1e9d)));
-        System.out.println("abort rate (%) = " + (aborted * 100 / ((double) n)));
-    }
-
-    public static void populate() throws Exception {
-        Connection c = DriverManager.getConnection(options.database, options.user, options.passwd);
-        Workload.populate(rand, c);
-        c.close();
-    }
-
-    public static void execute() throws Exception {
-        Benchmark[] r = new Benchmark[options.clients];
-        for (int i = 0; i < r.length; i++)
-            r[i] = new Benchmark();
+	
+	private static final Options options = new Options();
+	private static final Random rand = new Random();
+	private static boolean started, stopped;
+	private static int n, aborted;
+	private static long total;
+	
+	public synchronized static void startBench() {
+		started = true;
+		
+		System.out.println("Started!");
+	}
+	
+	public synchronized static void logTransaction(long tr, boolean success) {
+		if (started && !stopped) {
+			n++;
+			total += tr;
+			if (!success)
+				aborted += 1;
+		}
+	}
+	
+	public synchronized static boolean isStopped() {
+		return stopped;
+	}
+	
+	public synchronized static void stopBench() {
+		stopped = true;
+		
+		System.out.println("throughput (txn/s) = " + (n / ((double) options.runtime)));
+		System.out.println("response time (s) = " + (total / (n * 1e9d)));
+		System.out.println("abort rate (%) = " + (aborted * 100 / ((double) n)));
+	}
+	
+	public static void populate() throws Exception {
+		Connection c = DriverManager.getConnection(options.database, options.user, options.passwd);
+		Workload.populate(rand, c);
+		c.close();
+	}
+	
+	public static void execute() throws Exception {
+		Benchmark[] r = new Benchmark[options.clients];
+		for (int i = 0; i < r.length; i++)
+			r[i] = new Benchmark();
 		for (Benchmark benchmark : r) {
 			benchmark.start();
 		}
-        Thread.sleep(options.warmup * 1000); // aquecimento
-        startBench();
-        Thread.sleep(options.runtime * 1000);  // medidas!!!
-        stopBench();
+		Thread.sleep(options.warmup * 1000); // aquecimento
+		startBench();
+		Thread.sleep(options.runtime * 1000);  // medidas!!!
+		stopBench();
 		for (Benchmark benchmark : r) {
 			benchmark.join();
 		}
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        JCommander parser = JCommander.newBuilder().addObject(options).build();
-        try {
-            parser.parse(args);
-            if (options.help) {
-                parser.usage();
-                return;
-            }
-        } catch (Exception e) {
-            parser.usage();
-            return;
-        }
-
-        if (options.populate) populate();
-        if (options.execute) execute();
-    }
-
-    public void run() {
-        try {
-            Connection c = DriverManager.getConnection(options.database, options.user, options.passwd);
-            while (!isStopped()) {
-                long before = System.nanoTime();
-                boolean success = true;
-                try {
-                    Workload.transaction(rand, c);
-                } catch (SQLException e) {
-                    // check if it is an isolation-related exception
-                    // make sure other exceptions are shown
-                    if (e.getSQLState().startsWith("40")) {
-                        try {
-                            c.rollback();
-                        } catch (Exception ignored) {
-                        }
-                        success = false;
-                    } else
-                        throw e;
-                } finally {
-                    long after = System.nanoTime();
-                    logTransaction(after - before, success);
-                }
-            }
-            c.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		JCommander parser = JCommander.newBuilder().addObject(options).build();
+		try {
+			parser.parse(args);
+			if (options.help) {
+				parser.usage();
+				return;
+			}
+		} catch (Exception e) {
+			parser.usage();
+			return;
+		}
+		
+		if (options.populate) populate();
+		if (options.execute) execute();
+	}
+	
+	public void run() {
+		try {
+			Connection c = DriverManager.getConnection(options.database, options.user, options.passwd);
+			while (!isStopped()) {
+				long before = System.nanoTime();
+				boolean success = true;
+				try {
+					Workload.transaction(rand, c);
+				} catch (SQLException e) {
+					// check if it is an isolation-related exception
+					// make sure other exceptions are shown
+					if (e.getSQLState().startsWith("40")) {
+						try {
+							c.rollback();
+						} catch (Exception ignored) {
+						}
+						success = false;
+					} else
+						throw e;
+				} finally {
+					long after = System.nanoTime();
+					logTransaction(after - before, success);
+				}
+			}
+			c.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
--- benchmark/src/main/java/abd/Workload.java
+++ benchmark/src/main/java/abd/Workload.java
@@ -23,24 +23,21 @@ public class Workload {
 		
 		// drop tables, if they exist
 		s.executeUpdate("drop table if exists client cascade");
-		
 		s.executeUpdate("drop table if exists product cascade");
-
 		s.executeUpdate("drop table if exists invoice cascade");
-
 		s.executeUpdate("drop table if exists mv_product_sales cascade");
-
+		
+		// drop triggers, if they exist
 		s.executeUpdate("drop trigger if exists update_product_sales on abd.invoice");
-
+		
+		// drop functions, if they exist
 		s.executeUpdate("drop function if exists update_product_sales");
 		
-		// create tables and insert values
+		// create tables
 		s.executeUpdate("create table client (id serial primary key, name varchar, address varchar, data varchar)");
-		
 		s.executeUpdate("create table product (id serial primary key, description varchar, data varchar)");
-
 		s.executeUpdate("create table invoice (id serial primary key, product_id int, client_id int, data varchar)");
-
+		
 		// create indexes to account operation
 		s.executeUpdate("create index idx_invoice_client_id on invoice(client_id)");
 		s.executeUpdate("create index idx_invoice_product_id on invoice(product_id)");
@@ -54,24 +51,23 @@ public class Workload {
 		for (int i = 0; i < MAX; i++) {
 			s.executeUpdate("insert into product (description, data) values ('" + DESCRIPTION + "', '" + DATA + "')");
 		}
-
-		// create materialized view to top10 operation
+		
+		// create materialized view (plus insert trigger and function) to top10 operation
 		s.executeUpdate("select id as product_id, 0 as total_sales into mv_product_sales from product group by product_id");
-
+		
 		s.executeUpdate("create function update_product_sales() returns trigger as '" +
 				"	BEGIN" +
-				"		update mv_product_sales set total_sales = total_sales + 1 where product_id = new.product_id;"+
+				"		update mv_product_sales set total_sales = total_sales + 1 where product_id = new.product_id;" +
 				"		return new;" +
 				"	END" +
 				"' language 'plpgsql'"
 		);
-
+		
 		s.executeUpdate("create trigger update_product_sales" +
 				"	after insert on invoice" +
 				"	for each row execute procedure update_product_sales();"
 		);
-
-
+		
 		s.close();
 	}
 	
@@ -110,11 +106,10 @@ public class Workload {
 	
 	private static void account(Random rand, Statement s) throws Exception {
 		int clientId = generateId(rand);
-
+		
 		ResultSet rs = s.executeQuery("select description from invoice inner join product on product_id=product.id where client_id=" + clientId);
 		
 		while (rs.next()) {
-			;
 		}
 	}
 	
@@ -122,7 +117,6 @@ public class Workload {
 		ResultSet rs = s.executeQuery("select product_id from mv_product_sales order by total_sales desc limit 10;");
 		
 		while (rs.next()) {
-			;
 		}
 	}
 	
